0d972c6bd84a2a70b269f95724450a6caa8f2d1e
Phillipp Christians <phillipp.christians@uni-oldenburg.de>
2023-10-25 06:45:26 +0200

    icons

--- src/main/java/de/nachname/Main.java
+++ src/main/java/de/nachname/Main.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -35,7 +37,7 @@ public class Main extends Application {
         MenuItem xml = new MenuItem("XML");
         MenuItem jaxb = new MenuItem("JAXB");
         MenuItem serial = new MenuItem("Serialisieren");
-        Menu load = new Menu("L_aden");
+        Menu load = new Menu("La_den");
         Menu saveAP = new Menu("_Als Bild speichern");
         MenuItem print2 = new MenuItem("_Drucken");
         MenuItem changeSize = new MenuItem("_Größe ändern...");
@@ -73,25 +75,57 @@ public class Main extends Application {
         MenuBar menuBar = new MenuBar();
         menuBar.getMenus().addAll(editorMenu, territoriumMenu, elefantMenu, simulationMenu);
 
+        Image newImage = new Image("New24.gif");
+        ImageView newImageView = new ImageView(newImage);
+        Image loadImage = new Image("Open24.gif");
+        ImageView loadImageView = new ImageView(loadImage);
+        Image saveImage = new Image("Save24.gif");
+        ImageView saveImageView = new ImageView(saveImage);
+        Image compileImage = new Image("Compile24.gif");
+        ImageView compileImageView = new ImageView(compileImage);
+        Image gridImage = new Image("Terrain24.gif");
+        ImageView gridImageView = new ImageView(gridImage);
+        //views in order pls
+        Image deleteImage = new Image("Delete24.gif");
+        ImageView deleteImageView = new ImageView(deleteImage);
+        //
+        Image playImage = new Image("Play24.gif");
+        ImageView playImageView = new ImageView(playImage);
+        Image pauseImage = new Image("Pause24.gif");
+        ImageView pauseImageView = new ImageView(pauseImage);
+        Image stopImage = new Image("Stop24.gif");
+        ImageView stopImageView = new ImageView(stopImage);
+
+
         //ToolBar
         Button newT = new Button();
+        newT.setGraphic(newImageView);
         Button loadT = new Button();
+        loadT.setGraphic(loadImageView);
         Button saveT = new Button();
-        Button cleanT = new Button();
+        saveT.setGraphic(saveImageView);
+        Button compileT = new Button();
+        compileT.setGraphic(compileImageView);
         Button gridT = new Button();
+        gridT.setGraphic(gridImageView);
         Button eleT = new Button();
         Button peanutT = new Button();
         Button mouseT = new Button();
-        Button abortT = new Button();
-        Button takeT = new Button();
+        Button deleteT = new Button();
+        deleteT.setGraphic(deleteImageView);
+
         Button turnT = new Button();
         Button stepT = new Button();
+        Button takeT = new Button();
         Button playT = new Button();
+        playT.setGraphic(playImageView);
         Button pauseT = new Button();
+        pauseT.setGraphic(pauseImageView);
         Button stopT = new Button();
+        stopT.setGraphic(stopImageView);
 
         ToolBar toolbar = new ToolBar(
-                newT, loadT, new Separator(), saveT, cleanT, new Separator(), gridT, eleT, peanutT, mouseT, abortT, new Separator(), takeT, turnT, stepT, new Separator(), playT, pauseT, stopT
+                newT, loadT, new Separator(), saveT, compileT, new Separator(), gridT, eleT, peanutT, mouseT, deleteT, new Separator(), takeT, turnT, stepT, new Separator(), playT, pauseT, stopT
         );
 
 
